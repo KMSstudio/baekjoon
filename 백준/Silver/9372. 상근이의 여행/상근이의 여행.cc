851cca401bec8b23a1e861f7389da581011735f2
Kang Myeong Seok <68946300+KMSstudio@users.noreply.github.com>
2026-03-31 10:33:23 +0900

    [Silver IV] Title: 상근이의 여행, Time: 16 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/9372. 상근이의 여행/상근이의 여행.cc
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <cstring>
+#include <cstdlib>
+#include <climits>
+#include <string>
+#include <queue>
+using namespace std;
+typedef long long ll;
+
+#define PQUE_ASC(a, b)  ((a) > (b))
+#define PQUE_DEC(a, b)  ((a) < (b))
+#define SORT_ASC(a, b)  ((a) < (b))
+#define SORT_DEC(a, b)  ((a) > (b))
+
+typedef int                 i1;
+typedef pair< i1, i1 >      i2;
+
+typedef long long           d1;
+typedef pair<d1, d1>        d2;
+typedef tuple<d1, d1, d1>   d3;
+typedef pair< d1, d2 >      dc;
+
+template <typename T> using pque_asc = std::priority_queue<T, vector<T>, greater<T>>;
+template <typename T> using pque_dec = std::priority_queue<T, vector<T>, less<T>>;
+
+template <typename T> using que = std::queue<T>;
+template <typename T> using stk = std::queue<T>;
+
+template <typename T> using vec = std::vector<T>;
+template <typename T> using vvc = std::vector<std::vector<T>>;
+
+#define mp      std::make_pair
+#define mt      std::make_tuple
+#define ALL(v)  (v).begin(), (v).end()
+#define sot(v)  std::sort(ALL(v))
+#define rev(v)  std::reverse(ALL(v))
+
+void solve(void){
+    int a, b;
+    int n, m;
+    cin >> n >> m;
+    for(int i=0;i<m;i++){
+        cin >> a >> b;
+    }
+    cout << n-1 << '\n';
+}
+
+int main(void){ 
+    int TT = 1;
+    ios::sync_with_stdio(false); cin.tie(0);
+    cin >> TT;
+    for(int i=0;i<TT;i++){ solve(); }
+    return 0;
+}
